dcdda0993a0175eca93bea60391ec9d6830f7490
LeeKiWoong <littlemanner1@gmail.com>
2026-04-15 00:17:18 +0900

    [클래스 및 인터페이스 사용하기 + (개인적)싱글턴 사용해보기]OZCodingSchool_Assignment_0413
    2026년 4월 14일 재업 // 과제 내용은 클래스와 인터페이스 사용하기였지만 추가로 주말에 열심히 공부한 싱글턴도 사용해 봤습니다. 부모가 인터페이스를 상속받으면 자식은 인터페이스를 상속하지 않아도 자동으로 상속되는 줄 모르고 자식에게 달아준 오점이 있었습니다.. 하지만 그로 인해서 깨닫게 되었으니 다음부터는 꼭 이 부분 체크하고 넘어가도록 하겠습니다.

--- OZCodingSchool_Assignment/OZCodingSchool_Assignment/Program.cs
+++ OZCodingSchool_Assignment/OZCodingSchool_Assignment/Program.cs
@@ -6,80 +6,159 @@ using System.Threading.Tasks;
 
 namespace OZCodingSchool_Assignment
 {
-    internal class Program
+    internal class LeeKiWoong
     {
-        class BubbleSort
+        internal interface IJumpable
         {
-            public static void Bubble(int[] sequence)
-            {
-                int TradeNumber = 0;
-
-                for (int j = 0; j < sequence.Length - 1; j++)
-                {
-                    for (int i = 0; i < sequence.Length - 1; i++)
-                    {
-
-                        int FirstNumber = sequence[i];
-                        int SecondNumber = sequence[i + 1];
-                        int SaveNumber = SecondNumber;
-
-                        if (FirstNumber > SecondNumber)
-                        {
-                            TradeNumber++;
-                            SecondNumber = FirstNumber;
-                            FirstNumber = SaveNumber;
-                            sequence[i] = FirstNumber;
-                            sequence[i + 1] = SecondNumber;
-                        }
-                    }
-                }
-
-                Console.WriteLine($"교환 횟수 = {TradeNumber}");
-            }
+            void Jump();
         }
 
-        internal class SelectionSort
+        internal interface ITransformable
         {
-            public static void Selection(int[] sequence)
+            void Transform();
+        }
+
+        internal interface ITerrorable
+        {
+            void Terror();
+        }
+
+        internal class Ball : IJumpable, ITransformable, ITerrorable
+        {
+            public string Name { get; protected set; }
+            protected bool m_isTransform;
+
+
+            public Ball(string name)
             {
-                int TradeNumber = 0;
-
-                for (int j = 0; j < sequence.Length - 1; j++)
-                {
-                    int lowNumber = sequence[j];
-                    int choiceNumber = j;
-
-                    for (int i = j; i < sequence.Length; i++)
-                    {
-                        if (sequence[i] < lowNumber)
-                        {
-                            lowNumber = sequence[i];
-                            choiceNumber = i;
-                        }
-                    }
-
-                    TradeNumber++;
-                    sequence[choiceNumber] = sequence[j];
-                    sequence[j] = lowNumber;
-                }
-                Console.WriteLine($"교환 횟수 = {TradeNumber}");
-
+                Name = name;
+                m_isTransform = false;
             }
+
+            public virtual void Jump()
+            {
+                if (m_isTransform)
+                {
+                    UIManager.Instance.ViewingText($"{Name}은 하늘 높이 날아올랐다!!", $"{Name} : 날 가지고 놀던 너희 인간들에게 심판을 내려주마!!");
+                    return;
+                }
+
+                UIManager.Instance.ViewingText($"{Name}이 통통 튀었다.");
+            }
+            void ITransformable.Transform()
+            {
+                UIManager.Instance.ViewingText($"{Name}은 로봇으로 변신을 하였다.", "위잉 - 치킨");
+                m_isTransform = true;
+            }
+
+            public void Terror()
+            {
+                if (m_isTransform)
+                {
+                    UIManager.Instance.ViewingText($"{Name}은 사람에 대한 무차별 공격을 가하기 시작했다!", $"{Name} : 인간 시대의 끝이 도래했다!!");
+                    return;
+                }
+
+                UIManager.Instance.ViewingText($"{Name}이 이웃집의 창문을 깨부셨다!!", "이웃집 아주머니가 화가 난 거 같다!! 도망치자!!");
+            }
+
+        }
+
+        internal class SmallBall : Ball, IJumpable
+        {
+            public SmallBall(string name) : base(name)
+            {
+                m_isTransform = false;
+            }
+
+            public override void Jump()
+            {
+                UIManager.Instance.ViewingText($"{Name}가 통! 하고 높이 뛰어올랐다!");
+            }
+
         }
 
 
-        static void Main(string[] leekiwoong)
+
+
+        internal class UIManager
         {
-            int[] sortOne = new int[20] { 17, 1, 3, 9, 13, 4, 19, 7, 16, 14, 20, 6, 5, 10, 12, 18, 2, 15, 8, 11 };
+            private static UIManager m_instance;
+
+            private UIManager() { }
+
+            public static UIManager Instance
+            {
+                get
+                {
+                    if (m_instance == null)
+                    {
+                        m_instance = new UIManager();
+                    }
+                    return m_instance;
+                }
+            }
 
 
-            //BubbleSort.Bubble(sortOne);
 
-            SelectionSort.Selection(sortOne);
+            public void ViewingText(string textOne)
+            {
+                Console.WriteLine(textOne);
+                Console.WriteLine();
+            }
 
-            Console.WriteLine(string.Join(",", sortOne));
+            public void ViewingText(string textOne, string textTwo)
+            {
+                Console.WriteLine(textOne);
+                Console.WriteLine(textTwo);
+                Console.WriteLine();
+            }
 
+            public void ViewingText(string textOne, string textTwo, string textThree)
+            {
+                Console.WriteLine(textOne);
+                Console.WriteLine(textTwo);
+                Console.WriteLine(textThree);
+                Console.WriteLine();
+            }
 
+        }
+
+        internal class Program
+        {
+            static void Main(string[] leeKiWoong)
+            {
+                Ball ballOne = new Ball("축구공");
+                ballOne.Jump();
+                ballOne.Terror();
+
+                Ball ballTwo = new Ball("공인척하는 메가트론");
+                ballTwo.Jump();
+                ballTwo.Terror();
+
+                ((ITransformable)ballTwo).Transform();
+
+                ballTwo.Jump();
+                ballTwo.Terror();
+
+                SmallBall ballThree = new SmallBall("탁구공");
+                ballThree.Jump();
+
+                List<IJumpable> jumpableList = new List<IJumpable>();
+
+                jumpableList.Add(ballThree);
+                jumpableList.Add(ballTwo);
+                jumpableList.Add(ballOne);
+
+                foreach (IJumpable jumpable in jumpableList)
+                {
+                    if (jumpable is Ball)
+                    {
+                        jumpable.Jump();
+                    }
+
+                }
+            }
         }
     }
 }
