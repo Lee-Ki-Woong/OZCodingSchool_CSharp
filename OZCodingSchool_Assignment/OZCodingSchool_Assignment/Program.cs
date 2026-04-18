bf9adcbaf0611ec8a0563190c490b3d9ff9a1abd
LeeKiWoong <littlemanner1@gmail.com>
2026-04-18 22:20:33 +0900

    [알고리즘 직접 만들어보기 - 삽입정렬]OZCodingSchool_Assignment_0418(3)
    대략 1시간 20분 정도 걸린 것 같다!
    처음 짠 코드는 AI에게 평가해달라고 했더니 이 코드는 버블 정렬과 선택 정렬의 방식에서 조금 변형된 형태인 것 같고, 삽입 정렬은 아니라고 했다.
    그래서 삽입 정렬 알고리즘에 대해서 유튜브에서 다시 확인하고, 정렬된 집합에서 한칸씩 내려오면서 정렬하도록 바꾸었더니 AI가 삽입 정렬의 논리를 아주 잘 구현했다고 칭찬하였다.
    다만, AI는 매번 대입연산이 이루어지니 내 삽입 정렬은 완벽한 삽입 정렬은 아니고, 대입 연산은 딱 한번만 일어나게 바꾸라고 하는데 이건 나중에 천천히 바꿔봐야겠다.

--- OZCodingSchool_Assignment/OZCodingSchool_Assignment/Program.cs
+++ OZCodingSchool_Assignment/OZCodingSchool_Assignment/Program.cs
@@ -1,159 +1,107 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
-using System.Xml.Schema;
 
 namespace OZCodingSchool_Assignment
 {
-    public interface IDamageAble
-    {
-        string Name { get; }
-        int Damage { get; }
-    }
-
-    internal class Entity
-    {
-        public string Name { get; set; }
-        public int Damage { get; set; }
-
-        public Entity(string name, int damage)
-        {
-            Name = name;
-            Damage = damage;
-        }
-
-        
-    }
-
-    internal class Cocoa : Entity, IDamageAble
-    {
-        public event Action<string, int> OnDamaged;
-
-        public Cocoa(string name ,int damage) : base (name, damage) { }
-
-        public void BindDamagedEvent(Action<string, int> damaged)
-        {
-            OnDamaged += damaged;
-        }
-
-        public void CocoaDamaged(Func<bool> damaged, Action NoDamagedCallback)
-        {
-            bool isChoice = damaged();
-            
-            if(isChoice)
-            {
-                OnDamaged?.Invoke(Name, Damage);
-            }
-            else
-            {
-                NoDamagedCallback();
-            }
-        }
-    }
-
-    internal class SuperStone : Entity
-    {
-        public event Action<string, int> OnDamaged;
-
-        public SuperStone(string name, int damage) : base (name, damage) { }
-
-        public void BindDamagedEvent(Action<string, int> damaged)
-        {
-            OnDamaged += damaged;
-        }
-        public void SuperStoneDamaged(Func<bool> damaged, Action NoDamagedCallback)
-        {
-            bool isChoice = damaged();
-
-            if (isChoice)
-            {
-                OnDamaged?.Invoke(Name, Damage);
-            }
-            else
-            {
-                NoDamagedCallback();
-            }
-        }
-    }
-
-    internal class Damaged
-    {
-        private static Damaged m_instance;
-        private Damaged() { }
-        public static Damaged Instance
-        {
-            get
-            {
-                if(m_instance == null)
-                {
-                    m_instance = new Damaged();
-                }
-                return m_instance;
-            }
-        }
-
-
-        public bool CheckAttack()
-        {
-            Console.WriteLine("공격을 하시겠습니까?");
-            string choice = Console.ReadLine();
-
-            choice = choice.ToLower();
-
-            if (choice == "y")
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-
-        public void EntityDamaged<T>(string name, int damage) where T : IDamageAble
-        {
-            Console.WriteLine($"{name}은 {damage}만큼의 데미지를 받았다!!");
-        }
-
-        public void EntityDamaged(string name, int damage)
-        {
-            Console.WriteLine($"{name}은 데미지를 받지 않는다!! {damage}만큼의 데미지가 무시되었다!!");
-        }
-
-        public void EntityNoDamaged()
-        {
-            Console.WriteLine("데미지를 받지 않았다!!");
-        }
-    }
-
-    internal class Test
-    {
-        public void Ramda()
-        {
-            Action<string> asdf = (string aaa) => { Console.WriteLine("야호"); };
-        }
-    
-    }
-
     internal class Program
     {
-        static void Main(string[] leeKiWoong)
+        class BubbleSort
         {
-            Cocoa goodCocoa = new Cocoa("코코아", 10);
-            SuperStone BadSuperStone = new SuperStone("무적돌", 500);
+            public static void Bubble(int[] sequence)
+            {
+                int TradeNumber = 0;
+
+                for (int j = 0; j < sequence.Length - 1; j++)
+                {
+                    for (int i = 0; i < sequence.Length - 1; i++)
+                    {
+
+                        int FirstNumber = sequence[i];
+                        int SecondNumber = sequence[i + 1];
+                        int SaveNumber = SecondNumber;
+
+                        if (FirstNumber > SecondNumber)
+                        {
+                            TradeNumber++;
+                            SecondNumber = FirstNumber;
+                            FirstNumber = SaveNumber;
+                            sequence[i] = FirstNumber;
+                            sequence[i + 1] = SecondNumber;
+                        }
+                    }
+                }
+
+                Console.WriteLine($"교환 횟수 = {TradeNumber}");
+            }
+        }
+
+        internal class SelectionSort
+        {
+            public static void Selection(int[] sequence)
+            {
+                int TradeNumber = 0;
+
+                for (int j = 0; j < sequence.Length - 1; j++)
+                {
+                    int lowNumber = sequence[j];
+                    int choiceNumber = j;
+
+                    for (int i = j; i < sequence.Length; i++)
+                    {
+                        if (sequence[i] < lowNumber)
+                        {
+                            lowNumber = sequence[i];
+                            choiceNumber = i;
+                        }
+                    }
+
+                    TradeNumber++;
+                    sequence[choiceNumber] = sequence[j];
+                    sequence[j] = lowNumber;
+                }
+                Console.WriteLine($"교환 횟수 = {TradeNumber}");
+
+            }
+        }
+
+        internal class InsertionSort
+        {
+            public static void Insertion(int[] sequence)
+            {
+                for (int j = 1; j < sequence.Length; j++)
+                {
+                    int InsertNumber = sequence[j];
+
+                    for (int i = j - 1; i >= 0; i--)
+                    {
+                        int CheckNumber = sequence[i];
+                        
+
+                        if (InsertNumber < CheckNumber)
+                        {
+                            sequence[i] = InsertNumber;
+                            sequence[i+1] = CheckNumber;
+                        }
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] leekiwoong)
+        {
+            int[] sortOne = new int[20] { 17, 1, 3, 9, 13, 4, 19, 7, 16, 14, 20, 6, 5, 10, 12, 18, 2, 15, 8, 11 };
 
 
-            goodCocoa.BindDamagedEvent(Damaged.Instance.EntityDamaged<Cocoa>);
-            BadSuperStone.BindDamagedEvent(Damaged.Instance.EntityDamaged);
+            //BubbleSort.Bubble(sortOne);
 
+            //SelectionSort.Selection(sortOne);
 
-            goodCocoa.CocoaDamaged(Damaged.Instance.CheckAttack, Damaged.Instance.EntityNoDamaged);
-            BadSuperStone.SuperStoneDamaged(Damaged.Instance.CheckAttack, Damaged.Instance.EntityNoDamaged);
+            InsertionSort.Insertion(sortOne);
 
+            Console.WriteLine(string.Join(",", sortOne));
 
 
         }
